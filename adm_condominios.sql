6bc8ca95c3f3b518f0cc5487ef01c476d2fc9d5a
FernandoKGA <fernando.kga@gmail.com>
2019-03-28 01:58:48 -0300

    Adicionando outros files

--- adm_condominios.sql
+++ adm_condominios.sql
@@ -1,33 +1,3 @@
--- Database: adm_condominio
-
--- DROP DATABASE adm_condominio;
-
--- criação de database só pode ser executada sem transação?
-
-CREATE DATABASE adm_condominio
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Portuguese_Brazil.1252'
-    LC_CTYPE = 'Portuguese_Brazil.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-
--- rodar dentro do Query Tools da Database
-
-CREATE SCHEMA adm_condominio AUTHORIZATION postgres;
-GRANT ALL ON SCHEMA adm_condominio TO postgres;
-
-SET ROLE TO postgres;
-
--- mudando de schema
-
-SET search_path TO adm_condominio;
-
--- enumeradores
-
-CREATE TYPE tipo_assembleia AS ENUM('AGO','AGE');
-
 -- entidades fortes
 
 CREATE TABLE adm_condominio.Endereco (
@@ -117,7 +87,7 @@ CREATE TABLE adm_condominio.Documento (
 
 CREATE TABLE adm_condominio.Espaco (
   fk_id_condominio INT NOT NULL REFERENCES Condominio(id_condominio),
-  id_espaco SERIAL NOT NULL,
+  id_espaco SERIAL NOT NULL UNIQUE,
   nome_espaco VARCHAR(30) NOT NULL,
   capacidade INT NOT NULL,
   reservavel BOOLEAN NOT NULL,
