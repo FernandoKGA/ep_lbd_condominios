88f7bbe30bbb81c4f0f5d9c88986399fe680de55
FernandoKGA <fernando.kga@gmail.com>
2019-05-11 13:20:45 -0300

    modificando

--- regras_negocio_a/procedure_reserva_espaco.sql
+++ regras_negocio_a/procedure_reserva_espaco.sql
@@ -8,6 +8,7 @@ CREATE OR REPLACE PROCEDURE reserva_espaco(
 LANGUAGE plpgsql    
 AS $$
 BEGIN
+	IF 
 	IF (
 		SELECT COUNT(*) FROM adm_condominio.reserva
 		WHERE fk_id_pessoa = id_pessoa AND 
@@ -15,6 +16,13 @@ BEGIN
 	) = 0 THEN
 		INSERT INTO adm_condominio.reserva 
 		VALUES(id_pessoa,id_espaco,hora_inicial,hora_final,data_reserva);
+	ELSE IF (
+		SELECT COUNT(*) FROM adm_condominio.reserva
+		WHERE fk_id_pessoa = id_pessoa AND 
+		DATE_PART('month',data) = DATE_PART('month',data_reserva)
+	) < 3 THEN
+		IF (SELECT COUNT(*) FROM adm_condominio.reserva WHERE data = data_reserva AND
+		   fk_id_pessoa = id_pessoa) = 0 THEN
 	END IF;
 	COMMIT;
 END;
