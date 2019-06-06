425a27273be406048f1f6056c00854a528418700
FernandoKGA <fernando.kga@gmail.com>
2019-06-05 21:15:04 -0300

    Artefato A pronto

--- terceira_entrega/parte_a/inserts_tipos.sql
+++ terceira_entrega/parte_a/inserts_tipos.sql
@@ -1,3 +1,4 @@
+-- ((nome, tipo condominio),id_endereco)
 INSERT INTO adm_condominio.TipoCondominio(condominio,fk_id_endereco) 
 VALUES
 (('Ed. Molina','e'),31),
@@ -11,13 +12,14 @@ VALUES
 (('Ed. Jax','e'),39),
 (('Ed. Simpar','e'),40);
 
+-- pessoa(cpf,nome,data_nasciment,sexo)
 -- (((cpf,nome,data_nascimento,sexo),funcao,horario_entrada,horario_saida),fk_id_endereco),
 INSERT INTO adm_condominio.TipoFuncionario(funcionario,fk_id_condominio)
 VALUES
 ((('1133344488','João da Silva','15-04-1975','M'),'Zelador','08:00','17:00'),1),
 ((('2354233841','Rodrigues Lopes','03-06-1969','M'),'Porteiro','21:00','06:00'),2),
 ((('4233021304','James Logrado','03-12-1987','M'),'Faxineiro','10:00','18:00'),3)
---((('1133344488','João da Silva','15-04-1975','M'),'Zelador','08:00','17:00'),4),
+--(,4),
 --(,5),
 --(,6),
 --(,7),
